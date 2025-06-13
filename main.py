4c3d2c7724d15f418f63c95076cd844909edf478
Aleksey <ad.sychev22@gmail.com>
2025-06-13 16:37:39 +0300

    fixes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import asyncio
-from os import path, mkdir, getcwd
+from os import path, mkdir, getcwd, listdir
 
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-from aiohttp import ClientSession, TCPConnector
+from aiohttp import ClientConnectorError, ClientSession, TCPConnector
 import aiofiles
 
 
@@ -30,13 +30,20 @@ class cs:
         print(cs.RED + cs.add_tabs(text), cs.END)
 
 
-def create_dirs(name, month, day, offset):
+def create_dirs(name, url):
+    name = name.lower()
+
     if not path.isdir(f"{getcwd()}\\images"):
         mkdir(f"{getcwd()}\\images")
     if not path.isdir(f"{getcwd()}\\images\\{name}"):
         mkdir(f"{getcwd()}\\images\\{name}")
-    if not path.isdir(f"{getcwd()}\\images\\{name}\\{month}_{day}_{offset[1:]}"):
-        mkdir(f"{getcwd()}\\images\\{name}\\{month}_{day}_{offset[1:]}")
+    new_path = f"{getcwd()}\\images\\{name}\\{len(listdir(f"{getcwd()}\\images\\{name}\\")) + 1}"
+    if not path.isdir(new_path):
+        mkdir(new_path)
+    with open(new_path + "\\article_url.txt", "w") as file:
+        file.write(url)
+
+    return new_path
 
 
 async def download_photo(session: ClientSession, url: str, headers, download_path: str):
@@ -64,39 +71,49 @@ async def search_page(session: ClientSession, url: str, headers, day, month) -> 
             src = item.get('src')
             if not "http" in src:
                 photos.append(f"https://telegra.ph{src}")
+            else:
+                photos.append(src)
 
         return photos
 
 
-async def parse(session: ClientSession, name, day, month, offset):
+async def parse_article(session: ClientSession, name, day, month, offset):
+    global ALREADY_CHECKED_URLS
+
+    url = f"https://telegra.ph/{name}-{month}-{day}{offset}"
+    if url in ALREADY_CHECKED_URLS:
+        return
+
     HEADERS = {
         'User-Agent': User_Agent
     }
-    url = f"https://telegra.ph/{name}-{month}-{day}{offset}"
     photo_links = await search_page(session, url, HEADERS, day, month)
     if len(photo_links) > photos_required:
-        create_dirs(name, month, day, offset)
+        base_download_path = create_dirs(name, url)
         tasks = list()
         for i, i_link in enumerate(photo_links):
-            i_download_path = f"images/{name}/{month}_{day}_{offset[1:]}/{month}_{day}_{offset[1:]}_{i}.jpg"
+            i_download_path = base_download_path + f"\\{len(listdir(base_download_path)) + i + 1}.jpg"
             tasks.append(download_photo(session, i_link, HEADERS, i_download_path))
         await asyncio.gather(*tasks)
 
 
 async def main():
-    conn = TCPConnector(limit_per_host=10)
+    conn = TCPConnector(limit_per_host=50)
     async with ClientSession(trust_env=True, connector=conn) as session:
         for _month in range(1, 13):
             for _day in range(1, 32):
                 for _offset in range(1, offset + 1):
-                    if _offset == 1:
-                        await parse(session, name, f"{_day:02}", f"{_month:02}", "")
-                    else:
-                        await parse(session, name, f"{_day:02}", f"{_month:02}", f"-{_offset}")
+                    try:
+                        if _offset == 1:
+                            await parse_article(session, name, f"{_day:02}", f"{_month:02}", "")
+                        else:
+                            await parse_article(session, name, f"{_day:02}", f"{_month:02}", f"-{_offset}")
+                    except ClientConnectorError:
+                        continue
 
 
 if __name__ == "__main__":
-    name = input(f"{cs.INFO}Name: ")
+    name = input(f"{cs.INFO}Name: ").lower()
     offset = input("Offset (enter=1): ")
     offset = int(offset) if offset != '' else 1
     photos_required = input("Minimum photos to save (enter=1): ")
@@ -105,4 +122,5 @@ if __name__ == "__main__":
 
     User_Agent = UserAgent().random
 
+    ALREADY_CHECKED_URLS = set()
     asyncio.run(main())
